9643d3eb5ae1f72e569b7e25536e345b2bee76fc
Acbox <850625057@qq.com>
2023-08-25 18:54:05 +0800

    Update.

--- functionUsing/main.js
+++ functionUsing/main.js
@@ -2,11 +2,11 @@ import { Car, object, animation, interpolator } from "newcar"
 
 const car = new Car(document.getElementById("mycanvas"), 60);
 
-const func1 = new object.MathImage((x) => 100 * Math.sin(0.1 * x), 0, 0, {
+const func1 = new object.MathImage((x) => Math.sin(x), 0, 0, {
   color: "#FFFFFF",
   lineWidth: 3,
 });
-const func2 = new object.MathImage((x) => 100 * Math.cos(0.1 * x), 0, 0, {
+const func2 = new object.MathImage((x) => Math.cos(x), 0, 0, {
   color: "skyblue",
   lineWidth: 3,
 });
@@ -15,100 +15,81 @@ const system = new object.CoordinateSystem(0, 0, 0, 0, {
   y: 300,
   children: [func1, func2],
 });
-// const text = new Text("100·sin(0.1x) 100·cos(0.1x)", {
-//   size: 30,
-//   align: "center",
-//   x: 400,
-//   y: 300,
-//   transparency: 0,
-// });
+
 const math = new object.HTMLPlugin(`
 <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
   <mi>y</mi>
   <mo>=</mo>
-  <mn>100</mn>
-  <mo>&#x00D7;<!-- × --></mo>
   <mi>sin</mi>
   <mo>&#x2061;<!-- ⁡ --></mo>
-  <mn>0.1</mn>
   <mi>x</mi>
 </math>
 <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
   <mi>y</mi>
   <mo>=</mo>
-  <mn>100</mn>
-  <mo>&#x00D7;<!-- × --></mo>
   <mi>cos</mi>
   <mo>&#x2061;<!-- ⁡ --></mo>
-  <mn>0.1</mn>
   <mi>x</mi>
 </math>`
 , {
   transparency: 0,
   x: 300,
-  y: 225
+  y: 225,
+  scaleX: 2,
+  scaleY: 2
 })
 
-car.addObject(system).addObject(math);
+car.addObject(system, math);
 
 car
   .addAnimationItem(
-    new animation.AxisLength({
+    new animation.AxisLimit2d(system, {
       startAt: 0,
       lastsFor: 50,
       to: [600, 200, 0, 0],
-      bindTo: system,
       by: interpolator.easeOutSine,
     })
   )
   .addAnimationItem(
-    new animation.Limit({
+    new animation.Limit(func1, {
       startAt: 50,
       lastsFor: 800,
-      to: [0, 500],
-      bindTo: func1,
+      to: [0, 12],
     })
   )
   .addAnimationItem(
-    new animation.Limit({
+    new animation.Limit(func2, {
       startAt: 50,
       lastsFor: 800,
-      to: [0, 500],
-      bindTo: func2,
+      to: [0, 12],
     })
   )
   .addAnimationItem(
-    new animation.Scale({
+    new animation.Scale(system, {
       startAt: 450,
       lastsFor: 50,
       by: interpolator.easeOutSine,
       to: [0.5, 0.5],
-      bindTo: system,
     })
   )
   .addAnimationItem(
-    new animation.Translation({
+    new animation.Translation(system, {
       startAt: 450,
       lastsFor: 50,
       to: [400, 150],
-      bindTo: system,
     })
   )
   .addAnimationItem(
-    new animation.Transparency({
+    new animation.Transparency(math, {
       startAt: 500,
       lastsFor: 30,
       to: 1,
-      bindTo: math,
     })
   );
 
 car.play();
 
 car.exports(1, 850, (url) => {
-  const element = document.getElementById("video");
-  element.innerHTML = "Click here to jump to video file";
-  element.href = url;
   console.log(url);
 });
 
